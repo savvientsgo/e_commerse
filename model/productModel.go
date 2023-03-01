16b249965ead9606107f4e0bd6faba17539134ef
Dhara <121138188+dharaa8577@users.noreply.github.com>
2023-03-01 15:22:35 -0700

    Update productModel.go

--- model/productModel.go
+++ model/productModel.go
@@ -1,1 +1,16 @@
 package model
+
+import "time"
+
+type Inventory struct {
+	ID          string    `bson:"_id,omitempty"`
+	Name        string    `bson:"name"`
+	Type        string    `bson:"type"`
+	Description string    `bson:"description,omitempty"`
+	Price       float64   `bson:"price"`
+	Quantity    uint      `bson:"quantity"`
+	SellerID    string    `bson:"sellerId,omitempty"`
+	CreatedAt   time.Time `bson:"createdAt"`
+	UpdatedAt   time.Time `bson:"updatedAt"`
+	Condition   string    `bson:"condition,omitempty"`
+}
