46c2286dbe11d450d950a0218cb5b086c580b96a
Dhara <121138188+dharaa8577@users.noreply.github.com>
2023-03-01 15:23:27 -0700

    Update orderModel.go

--- model/orderModel.go
+++ model/orderModel.go
@@ -1,1 +1,20 @@
 package model
+import (
+	"time"
+)
+
+type Order struct {
+	ID            string    `bson:"_id,omitempty"`
+	UserID        string    `bson:"userId,omitempty"`
+	InventoryID   string    `bson:"inventoryId,omitempty"`
+	BuyerID       string    `bson:"buyerId,omitempty"`
+	SellerID      string    `bson:"sellerId,omitempty"`
+	Quantity      uint      `bson:"quantity"`
+	TotalPrice    float64   `bson:"totalPrice"`
+	PaymentMethod string    `bson:"paymentMethod,omitempty"`
+	OrderDate     time.Time `bson:"orderDate"`
+	DeliveryDate  time.Time `bson:"deliveryDate,omitempty"`
+	Address       *Address  `bson:"address,omitempty"`
+	CreatedAt     time.Time `bson:"createdAt"`
+	UpdatedAt     time.Time `bson:"updatedAt"`
+}
