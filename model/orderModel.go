400796c797eca39fbcdd7ee71c51014a3f7cad5e
Dhara <121138188+dharaa8577@users.noreply.github.com>
2023-03-01 16:04:39 -0700

    Update orderModel.go

--- model/orderModel.go
+++ model/orderModel.go
@@ -9,12 +9,12 @@ type Order struct {
 	InventoryID   string    `bson:"inventoryId,omitempty"`
 	BuyerID       string    `bson:"buyerId,omitempty"`
 	SellerID      string    `bson:"sellerId,omitempty"`
-	Quantity      uint      `bson:"quantity"`
+	Quantity       int      `bson:"quantity"`
 	TotalPrice    float64   `bson:"totalPrice"`
 	PaymentMethod string    `bson:"paymentMethod,omitempty"`
 	OrderDate     time.Time `bson:"orderDate"`
 	DeliveryDate  time.Time `bson:"deliveryDate,omitempty"`
-	Address       *Address  `bson:"address,omitempty"`
+	Address        Address  `bson:"address,omitempty"`
 	CreatedAt     time.Time `bson:"createdAt"`
 	UpdatedAt     time.Time `bson:"updatedAt"`
 }
