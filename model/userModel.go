b8eb37216c4666733e54ab64400ef12f67ce1215
Dhara <121138188+dharaa8577@users.noreply.github.com>
2023-03-03 11:09:49 -0700

    Update userModel.go

--- model/userModel.go
+++ model/userModel.go
@@ -2,36 +2,37 @@ package model
 
 import (
 	"time"
-
+        "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct {
-	ID           primitive.ObjectID `bson:"_id,omitempty"`
-	FirstName    string             `bson:"firstname"`
-	LastName     string             `bson:"lastname"`
-	Email        string             `bson:"email"`
-	UserName     string             `bson:"username"`
-	PasswordHash string             `bson:"passwordHash,omitempty"`
-	Address      Address            `bson:"address,omitempty"`
-	BankDetail   BankDetail         `bson:"bankDetail,omitempty"`
-	CreatedAt    time.Time          `bson:"createdAt"`
-	UpdatedAt    time.Time          `bson:"updatedAt"`
+	ID           primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	FirstName    string             `json:"firstName" bson:"firstName"`
+	LastName     string             `json:"lastName" bson:"lastName"`
+	Email        string             `json:"email" bson:"email"`
+	UserName     string             `json:"userName" bson:"userName"`
+	PasswordHash string             `json:"passwordHash" bson:"passwordHash,omitempty"`
+	Address      Address            `json:"address" bson:"address,omitempty"`
+	BankDetail   BankDetail         `json:"bankDetail" bson:"bankDetail,omitempty"`
+	CreatedAt    time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt    time.Time          `json:"updatedAt" bson:"updatedAt"`
 }
 
 //  Address and BankDetails
 
 type Address struct {
-	Street     string `bson:"street,omitempty"`
-	City       string `bson:"city,omitempty"`
-	State      string `bson:"state,omitempty"`
-	Country    string `bson:"country,omitempty"`
-	PostalCode string `bson:"postalCode,omitempty"`
+	Street     string `json:"street" bson:"street,omitempty"`
+	City       string `json:"city" bson:"city,omitempty"`
+	State      string `json:"state" bson:"state,omitempty"`
+	Country    string `json:"country" bson:"country,omitempty"`
+	PostalCode string `json:"postalCode" bson:"postalCode,omitempty"`
 }
 
 type BankDetail struct {
-	BankName    string `bson:"bankName,omitempty"`
-	AccountName string `bson:"accountName,omitempty"`
-	AccountNo   string `bson:"accountNo,omitempty"`
-	RoutingNo   string `bson:"routingNo,omitempty"`
+	BankName    string `json:"bankName" bson:"bankName,omitempty"`
+	AccountName string `json:"accountName" bson:"accountName,omitempty"`
+	AccountNo   string `json:"accountNo" bson:"accountNo,omitempty"`
+	RoutingNo   string `json:"routingNo" bson:"routingNo,omitempty"`
 }
+
